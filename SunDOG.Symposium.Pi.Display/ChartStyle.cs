81655e95f85ad5c88d98301e837da619e4dfa484
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-03-11 00:17:02 -0400

    Added some styling to the graphs
    - Added some styling to the graphs
    - Not tested yet

---
+++ SunDOG.Symposium.Pi.Display/ChartStyle.cs
@@ -0,0 +1,25 @@
+﻿namespace SunDOG.Symposium.Pi.Display
+{
+    public class ChartStyle
+    {
+
+        // --- COLORS ---
+        public string PrimaryColor { get; set; } = "#FBE3D6";
+        public string SecondaryColor { get; set; } = "#ff7f0e";
+        public string GridLineColor { get; set; } = "#0E0A1F";
+        public string AxisTitleColor { get; set; } = "#0E0A1F";
+
+        // --- STROKES ---
+        public double LineWidth { get; set; } = 2.0;
+        public string GridLineDashArray { get; set; } = "5, 5";
+
+        // --- FONTS ---
+        public string FontFamily { get; set; } = "Arial, sans-serif";
+        public double TitleFontSize { get; set; } = 25.0;
+        public double LabelFontSize { get; set; } = 18.0;
+
+        // --- SPACING ---
+        public int AxisPadding { get; set; } = 20;
+
+    }
+}
